cc4eb415fa2ab696b75fe1fe505607ec317b6318
newstockolm <vitot2002@gmail.com>
2018-04-10 14:20:29 +0200

    zbrisu nepotrebno v main

--- main.py
+++ main.py
@@ -59,54 +59,5 @@ def register():
         session['user'] = baza.dobi_uporabnika(user_id)
         return redirect('/')
 
-@app.route("/druga_stran", methods=['GET', 'POST'])
-def druga():
-    if request.method == 'POST':
-        session['user'] = baza.dobi_uporabnika(
-            username=request.form['username'],
-            password=request.form['password'])
-
-    return render_template("druga_stran.html")
-
-@app.route("/vislice/<znak>")
-def ugibaj(znak):
-    if len(znak) == 1 and znak.isalpha():
-        session['ugibal'] += znak
-        if znak not in session['beseda']:
-            session['slika'] += 1
-    for crka in session['beseda']:
-        if crka not in session['ugibal']:
-            break
-    else:
-        if session.get('user') is not None:
-            baza.vstavi_novo_igro(session['user'][0], session['slika'], session['beseda'])
-        return render_template('zmaga.html')
-
-    return render_template("vislice.html", vse_crke=ascii_crke)
-
-@app.route("/vislice")
-def vislice():
-    session['beseda'] = "endlessness"
-    with open('besede.txt') as besede:
-        for i in range(randint(1, 350747)):
-            session['beseda'] = besede.readline().strip()
-    session['slika'] = 0
-    session['ugibal'] = ''
-    return render_template("vislice.html", vse_crke=ascii_crke)
-
-@app.route("/blog/<int:st_blog>")
-def blog(st_blog):
-    zadnji = request.cookies.get("blog_stevilka")
-    result = render_template(
-        "blog.html", stevilka=st_blog, prejsni=zadnji)
-    result = make_response(result)
-    result.set_cookie("blog_stevilka", str(st_blog))
-    return result
-
-@app.route("/lestvica")
-def lestvica():
-    najboljsi = baza.dobi_najboljse()
-    return render_template("lestvica.html", najboljsi=najboljsi)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
